3d3cbef176082f3eb27b78c7eff3ab31898794e9
tianwenyuan <verycurious@163.com>
2019-02-25 17:19:23 +0800

    Initial commit

---
+++ ios/CalendarManager.h
@@ -0,0 +1,14 @@
+//
+//  CalendarManager.h
+//  ReactNativeCall
+//
+//  Created by user on 2019/2/25.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+// CalendarManager.h
+#import <React/RCTBridgeModule.h>
+
+@interface CalendarManager : NSObject <RCTBridgeModule>
+
+@end
